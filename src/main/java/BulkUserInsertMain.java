b5aacab558a17430d007d5113b1c568501f66bc0
jpereira <jpereira@geocom.com.uy>
2019-06-13 07:59:55 -0300

    Add connection pooling

--- src/main/java/BulkUserInsertMain.java
+++ src/main/java/BulkUserInsertMain.java
@@ -1,37 +1,61 @@
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
 public class BulkUserInsertMain {
+    private static final int BLOCKS = 1000;
+    private static final int BLOCK_SIZE = 1000;
 
     public static void main(String[] args) throws Exception {
         new BulkUserInsertMain().run();
     }
 
+    private DataSource createPool() {
+        BasicDataSource pool = new BasicDataSource();
+        pool.setUrl("jdbc:mysql://localhost/test2");
+        pool.setUsername("root");
+        pool.setPassword("geocom");
+        pool.setMaxWaitMillis(5000);
+        pool.setMaxIdle(4);
+        pool.setMaxTotal(10);
+        pool.setTimeBetweenEvictionRunsMillis(60000);
+        return pool;
+    }
+
     private void run() throws Exception {
-        long initTime = System.currentTimeMillis();
-        try (Connection connection = DriverManager
-                .getConnection("jdbc:mysql://localhost/test2",
-                        "root", "geocom")) {
-            for (int count = 0; count <= 100000; count++) {
-                    if (count % 10000 == 0)
-                        connection.setAutoCommit(false);
-                    try (PreparedStatement stmt = connection.prepareStatement(
-                            "insert into employee (emp_id, fname, lname) values (?,?,?)")) {
-                        stmt.setInt(1, count);
-                        stmt.setString(2,"Name " + count);
-                        stmt.setString(3, "Lname " + count);
+        DataSource datasource = createPool();
 
-                        stmt.execute();
+            for (int blockCount = 0; blockCount <= BLOCKS; blockCount++) {
 
+                long initTime = System.currentTimeMillis();
+
+
+                try (Connection connection = datasource.getConnection()) {
+
+                    connection.setAutoCommit(false);
+
+
+                    for (int count = 0; count < BLOCK_SIZE; count ++) {
+                        int id = blockCount*BLOCKS*BLOCK_SIZE + count;
+                        try (PreparedStatement stmt = connection.prepareStatement(
+                                "insert into employee (emp_id, fname, lname) values (?,?,?)")) {
+                            stmt.setInt(1, id);
+                            stmt.setString(2, "Name " + id);
+                            stmt.setString(3, "Lname " + id);
+
+                            stmt.execute();
+
+                        }
                     }
 
-                    if (count % 10000 == 0)
-                        connection.commit();
+                    connection.commit();
+                    System.out.println("Cantidad : " + ((blockCount + 1)*BLOCK_SIZE) + " tiempo en millis :" + (System.currentTimeMillis() - initTime));
 
-                    if (count % 10000 == 0)
-                        System.out.println("Cantidad : " + count + " tiempo en millis :" + (System.currentTimeMillis() - initTime));
                 }
+
             }
         }
 }
